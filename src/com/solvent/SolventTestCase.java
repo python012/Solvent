1f8481e7b63fa3ace3818f95fa8d9755ea846b7b
Reed Xia <huaqin2005@gmail.com>
2017-04-19 14:56:20 +0800

    add SolventToolkit class.

--- src/com/solvent/SolventTestCase.java
+++ src/com/solvent/SolventTestCase.java
@@ -24,91 +24,72 @@ import javax.imageio.ImageIO;
 import com.solvent.timerHelper.Timer;
 import com.solvent.l10nHelper.I18NUtil;
 import com.solvent.SolventLogger;
+import com.solvent.SolventToolkit;
 
 /**
  * Created by reed on 16/10/13.
  */
 public abstract class SolventTestCase {
 
-    private static ArrayList<Timer> timers = null;
-    private static ArrayList<CheckPoint> checkPoints = null;
-    protected static Logger log;
-    private final File directory = new File("/Users/reed/Documents/tmp1");
+	private static ArrayList<Timer> timers = null;
+	private static ArrayList<CheckPoint> checkPoints = null;
+	protected static Logger log;
+	private final File directory = new File("/Users/reed/Documents/tmp1");
 
-    public SolventTestCase() {
-        log = SolventLogger.getLogger(this.getClass());
-        I18NUtil.processI18NKeys(this);
-        directory.mkdir();
-    }
+	public SolventTestCase() {
+		log = SolventLogger.getLogger(this.getClass());
+		I18NUtil.processI18NKeys(this);
+		directory.mkdir();
+	}
 
-    private File filenameFor(Description description) {
-        String className = description.getClassName(); // get the test class
-                                                       // name
-        String methodName = description.getMethodName();
+	private File filenameFor(Description description) {
+		String className = description.getClassName();
+		String methodName = description.getMethodName();
+		return new File(directory, className + "_" + methodName + ".png");
+	}
+	
+	@Rule
+	public TestRule watcher = new TestWatcher() {
+		@Override
+		public void failed(Throwable e, Description description) {
+			SolventToolkit.silentlySaveScreenshotTo(filenameFor(description), "png");
+		}
+	};
 
-        return new File(directory, className + "_" + methodName + ".png");
-    }
+	@BeforeClass
+	public void setupClass() {
+		startHSQL();
+	}
 
-    private void silentlySaveScreenshotTo(File file, String format) {
-        try {
-            saveScreenshotTo(file, format);
-        } catch (Exception e) {
-            System.err.println("Failed to screenshot to " + file + ", " + e);
-        }
-    }
+	@Before
+	public void setup() {
+		checkPoints = new ArrayList<CheckPoint>();
+		timers = new ArrayList<Timer>();
+		start();
+	}
 
-    private static BufferedImage BufferedImagetakeScreenshot() throws AWTException {
-        Robot robot = new Robot();
-        Rectangle captureSize = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
-        return robot.createScreenCapture(captureSize);
-    }
+	@After
+	public void tearDown() throws Exception {
+		checkForFailures();
+		checkTimers();
+	}
 
-    private static void saveScreenshotTo(File file, String format) throws AWTException, IOException {
-        ImageIO.write(takeScreenshotTo(), format, file);
-    }
+	@AfterClass
+	public static void tearDownClass() {
+		HsqldbHelper.stopHSQL();
+	}
 
-    @Rule
-    public TestRule watcher = new TestWatcher() {
-        @Override
-        public void failed(Throwable e, Description description) {
-            silentlySaveScreenshotTo(filenameFor(description), "png");
-        }
-    };
+	protected abstract void start();
 
-    @BeforeClass
-    public void setupClass() {
-        startHSQL();
-    }
+	public String uniquify(String str) {
+		return str + "_" + System.currentTimeMillis();
+	}
 
-    @Before
-    public void setup() {
-        checkPoints = new ArrayList<CheckPoint>();
-        timers = new ArrayList<Timer>();
-        start();
-    }
-
-    @After
-    public void tearDown() throws Exception {
-        checkForFailures();
-        checkTimers();
-    }
-
-    @AfterClass
-    public static void tearDownClass() {
-        HsqldbHelper.stopHSQL();
-    }
-
-    protected abstract void start();
-
-    public String uniquify(String str) {
-        return str + "_" + System.currentTimeMillis();
-    }
-
-    public static void waitfor(long milles) {
-        try {
-            Thread.sleep(milles);
-        } catch (Exception e) {
-            log.info("\n\n timed out with exception: " + e.getMessage());
-        }
-    }
+	public static void waitfor(long milles) {
+		try {
+			Thread.sleep(milles);
+		} catch (Exception e) {
+			log.info("\n\n timed out with exception: " + e.getMessage());
+		}
+	}
 }
---
+++ src/com/solvent/SolventToolkit.java
@@ -0,0 +1,32 @@
+package com.solvent;
+
+import java.awt.AWTException;
+import java.awt.Rectangle;
+import java.awt.Robot;
+import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
+public class SolventToolkit {
+
+	public static void silentlySaveScreenshotTo(File file, String format) {
+		try {
+			saveScreenshotTo(file, format);
+		} catch (Exception e) {
+			System.err.println("Failed to screenshot to " + file + ", " + e);
+		}
+	}
+
+	private static BufferedImage takeScreenshot() throws AWTException {
+		Robot robot = new Robot();
+		Rectangle captureSize = new Rectangle(Toolkit.getDefaultToolkit().getScreenSize());
+		return robot.createScreenCapture(captureSize);
+	}
+
+	private static void saveScreenshotTo(File file, String format) throws AWTException, IOException {
+		ImageIO.write(takeScreenshot(), format, file);
+	}
+}
