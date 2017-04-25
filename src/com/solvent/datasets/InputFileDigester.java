7f8b36f16223ce7ad64a7fdc24a23e4f63dcd294
Reed Xia <huaqin2005@gmail.com>
2017-04-25 21:34:17 +0800

    Added functions in InputFileDigester.

--- src/com/solvent/datasets/InputFileDigester.java
+++ src/com/solvent/datasets/InputFileDigester.java
@@ -1,6 +1,13 @@
 package com.solvent.datasets;
 
+import java.io.InputStream;
+import java.util.ArrayList;
+
 import org.apache.log4j.Logger;
+import org.dom4j.Document;
+import org.dom4j.DocumentException;
+import org.dom4j.Element;
+import org.dom4j.io.SAXReader;
 
 import com.solvent.SolventLogger;
 
@@ -8,11 +15,44 @@ public class InputFileDigester {
 	private static final Logger log = SolventLogger.getLogger(InputFileDigester.class.getName());
 	private Document doc;
 	private SolventTestDataSet workingDataSet;
-	
-	
-	
-	public InputFileDigester() {
-		// TODO Auto-generated constructor stub
+
+	public InputFileDigester(InputStream in) {
+		SAXReader reader = new SAXReader();
+		try {
+			doc = reader.read(in);
+		} catch (DocumentException e) {
+			log.error("Error when trying to parse input file.", e);
+		}
 	}
 
+	public SolventTestDataSet getWorkingDataSet() {
+		return workingDataset;
+	}
+
+	public ArrayList<SolventTestDataSet> parseDataSets(String suiteDataSetName) {
+		ArrayList<SolventTestDataSet> dataSetCollection = new ArrayList<SolventTestDataSet>();
+		Element workingDS = (Element)this.doc.selectSingleNode("/testConfig/workingDataSet");
+		String workingDSName = null;
+
+		if (suiteDataSetName != null && suiteDataSetName.trim().length() != 0) {
+			workingDSName = suiteDataSetName.trim();
+		} else {
+			if (workingDS != null) {
+				workingDSName = workingDS.attributeValue("name");
+			}
+		}
+		log.debug("========== Working Data Set: " + workingDSName);
+		
+		if (workingDSName != null) {
+			SolventTestCase.setDataSetOverride(workingDSName);
+		}
+	}
+
+	
+	
+	
+	
+	
+	
+	
 }
