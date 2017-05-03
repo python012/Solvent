23e9c0f27d158554bad623f2c240da69b197ce3f
Reed Xia <huaqin2005@gmail.com>
2017-05-03 16:42:51 +0800

    Hot fix against ConfigurationError.

--- src/com/solvent/exception/ConfigurationError.java
+++ src/com/solvent/exception/ConfigurationError.java
@@ -3,15 +3,8 @@ package com.solvent.exception;
 import java.io.IOException;
 
 public class ConfigurationError extends Exception {
-	private String message;
 	
-	public ConfigurationError(String message) {
-		super(message);
-		this.message = message;
-	}
-
 	public ConfigurationError(String message, IOException e) {
-		super(message + "\n" + e.getMessage());
-		this.message = message + "\n" + e.getMessage();
+		super(message, e);
 	}
 }
