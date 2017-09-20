cbae7cf59c2cc87457ffb77fecd2bb176ee33861
Reed Xia <huaqin2005@gmail.com>
2017-09-20 22:50:19 +0800

    Added SolventTestDataSet class and InvalidFileException.

---
+++ src/com/solvent/datasets/SolventTestDataSet.java
@@ -0,0 +1,64 @@
+package com.solvent.datasets;
+
+import java.io.File;
+import java.security.InvalidParameterException;
+import java.util.ArrayList;
+
+import com.solvent.exception.InvalidFileException;
+
+public class SolventTestDataSet {
+	private SolventTestDataSetBean myBean;
+
+	public SolventTestDataSet(SolventTestDataSetBean bean) {
+		this.myBean = bean;
+	}
+
+	public String getName() {
+		return myBean.getName();
+	}
+
+	public String getVariable(String varName) {
+		String varValue = myBean.getVarValue(varName);
+		if (null == varValue) {
+			throw new InvalidParameterException("Input variable \"" + varName + "\" not found");
+		}
+		return varValue;
+	}
+	
+	public ArrayList<String> getVariables(String listName) {
+		ArrayList<String> vars = myBean.getVarValues(listName);
+		if (null == vars) {
+			throw new InvalidParameterException("Input variables list \"" + listName + "\" not found");
+		}
+		return vars;
+	}
+	
+	public File getFile(String fileName) throws InvalidFileException {
+		String filePath = myBean.getFile(fileName);
+		if (null == filePath) {
+			throw new InvalidParameterException("Input file variable \"" + filePath + "\" not found");
+		}
+		File file = new File(filePath);
+		if (!file.exists()) {
+			throw new InvalidFileException("Input file \"" + filePath + "\" not found");
+		}
+		return file;
+	}
+	
+	public ArrayList<File> getFiles(String listName) throws InvalidFileException {
+		ArrayList<String> filePaths = myBean.getFiles(listName);
+		if (null == filePaths) {
+			throw new InvalidParameterException("Input list variables \"" + listName + "\" not found");
+		}
+		ArrayList<File> files = new ArrayList<File>();
+		for (String filePath : filePaths) {
+			File file = new File(filePath);
+			if (!file.exists()) {
+				throw new InvalidFileException("Input file \"" + filePath + "\" not found");
+			}
+			files.add(file);
+		}
+		return files;
+	}
+}
+
---
+++ src/com/solvent/exception/InvalidFileException.java
@@ -0,0 +1,9 @@
+package com.solvent.exception;
+
+public class InvalidFileException extends Exception{
+	private String message;
+	
+	public InvalidFileException(String message) {
+		this.message = message;
+	}
+}
