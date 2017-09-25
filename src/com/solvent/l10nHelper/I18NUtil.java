6c098eae7cff05b2fd780ade2c4add0333477ce8
Reed Xia <huaqin2005@gmail.com>
2017-09-25 13:58:27 +0800

    Add processI18NKeys().

--- src/com/solvent/l10nHelper/I18NUtil.java
+++ src/com/solvent/l10nHelper/I18NUtil.java
@@ -1,10 +1,16 @@
 package com.solvent.l10nHelper;
 
+import com.solvent.Solvent;
 import com.solvent.SolventTestCase;
 
 public class I18NUtil {
 	public static void processI18NKeys(SolventTestCase testCase) {
 		
+	}
+
+	public static void processI18NKeys(Solvent solvent) {
+		// TODO Auto-generated method stub
+
 	}
 
 }
